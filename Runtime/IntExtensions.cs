68bc8b822be70d5e58887f1824bb338f51e0eaee
Mirzipan <mfarkas@pixelfederation.com>
2023-01-12 20:29:50 +0100

    ✨ added int.ToRoman

--- Runtime/IntExtensions.cs
+++ Runtime/IntExtensions.cs
@@ -4,6 +4,8 @@ namespace Mirzipan.Extensions
 {
     public static class IntExtensions
     {
+        #region TimeSpan
+
         public static TimeSpan Days(this int @this)
         {
             return TimeSpan.FromDays(@this);
@@ -28,5 +30,31 @@ namespace Mirzipan.Extensions
         {
             return TimeSpan.FromMilliseconds(@this);
         }
+
+        #endregion TimeSpan
+
+        #region Roman Numerals
+
+        private static readonly string[] I = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        private static readonly string[] X = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        private static readonly string[] C = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        private static readonly string[] M = { "", "M", "MM", "MMM", "" };
+
+        /// <summary>
+        /// Converts a number to roman representation (VII, IX, CD, CM, MMMXXVIII, etc.)
+        /// </summary>
+        /// <param name="this">Must be a number between 1 and 3999</param>
+        /// <returns></returns>
+        public static string ToRoman(this int @this)
+        {
+            if (@this is <= 0 or >= 4000)
+            {
+                return string.Empty;
+            }
+
+            return $"{M[@this / 1000]}{C[@this % 1000 / 100]}{X[@this % 100 / 10]}{I[@this % 10]}";
+        }
+
+        #endregion Roman Numerals
     }
 }

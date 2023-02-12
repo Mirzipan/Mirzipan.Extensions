bd719986ad9cbe85aa83147677e04b7483cfa3c0
Mirzipan <mfarkas@pixelfederation.com>
2023-02-12 19:44:55 +0100

    ✨added extensions for float and int using Mathf

---
+++ Runtime/Unity/FloatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Mirzipan.Extensions.Unity
+{
+    public static class FloatExtensions
+    {
+        public static float Clamp(this float @this, float min, float max)
+        {
+            return Mathf.Clamp(@this, min, max);
+        }
+        
+        public static float Clamp01(this float @this)
+        {
+            return Mathf.Clamp01(@this);
+        }
+    }
+}
---
+++ Runtime/Unity/IntExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Mirzipan.Extensions.Unity
+{
+    public static class IntExtensions
+    {
+        public static int Clamp(this int @this, int min, int max)
+        {
+            return Mathf.Clamp(@this, min, max);
+        }
+    }
+}

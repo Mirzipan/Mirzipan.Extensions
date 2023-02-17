ee21e3789093f8deb0d6ec2ee02d6efe1417c6ea
Mirzipan <mfarkas@pixelfederation.com>
2023-02-17 19:24:59 +0100

    ✨added setters for Slider min and max values

--- Runtime/Unity/UI/SliderExtensions.cs
+++ Runtime/Unity/UI/SliderExtensions.cs
@@ -15,5 +15,31 @@ namespace Mirzipan.Extensions.Unity.UI
             @this.onValueChanged.RemoveAllListeners();
             @this.onValueChanged.AddListener(listener);
         }
+
+        /// <summary>
+        /// Sets the min and max values for this <see cref="Slider"/>.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static void SetMinMax(this Slider @this, float min, float max)
+        {
+            @this.wholeNumbers = false;
+            @this.minValue = min;
+            @this.maxValue = max;
+        }
+
+        /// <summary>
+        /// Sets the min and max values for this <see cref="Slider"/>.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static void SetMinMax(this Slider @this, int min, int max)
+        {
+            @this.wholeNumbers = true;
+            @this.minValue = min;
+            @this.maxValue = max;
+        }
     }
 }
